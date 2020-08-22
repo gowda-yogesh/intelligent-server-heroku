ea951929ea150d310183e6a90cfc1b97b2065972
YOGESH C GOWDA <yog.161096@gmail.com>
2020-08-22 17:14:11 +0530

    bcrypt and knex

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const knex = require("knex");
+const PORT = process.env.PORT || 5000;
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'test',
+    database: 'smartbrain'
+  }
+});
 
 express()
   .use(bodyParser.json())
@@ -11,33 +23,33 @@ express()
 
     res.json("signin page open ");
     // console.log("post signin ", ++p);
-    // // console.log("signin req.body", JSON.parse(req.body));
+    // console.log("signin req.body", JSON.parse(req.body));
 
-    // console.log(req.body);
+    console.log(req.body);
 
-    // db
-    //     .select('email', 'hash')
-    //     .from('login')
-    //     .where('email', '=', req.body.email)
-    //     .then(user => {
-    //         console.log("hash value  = ", user);
-    //         console.log("hash value  = ", user[0].hash);
-    //         const isValid = bcrypt.compareSync(req.body.password, user[0].hash);
-    //         console.log("Validation = ", isValid);
-    //         if (isValid) {
-    //             return db.select('*')
-    //                 .from('users')
-    //                 .where('email', '=', req.body.email)
-    //                 .then(user => {
-    //                     res.json(user[0])
-    //                 })
-    //                 .catch(err => res.status(400).json('unable to get users'))
+    db
+      .select('email', 'hash')
+      .from('login')
+      .where('email', '=', req.body.email)
+      .then(user => {
+        console.log("hash value  = ", user);
+        console.log("hash value  = ", user[0].hash);
+        const isValid = bcrypt.compareSync(req.body.password, user[0].hash);
+        console.log("Validation = ", isValid);
+        if (isValid) {
+          return db.select('*')
+            .from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+              res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get users'))
 
-    //         } else {
-    //             return res.status(400).json('wrong credentials plz try again')
-    //         }
-    //     })
-    //     .catch(err => console.log("error = ", err));
+        } else {
+          return res.status(400).json('wrong credentials plz try again')
+        }
+      })
+      .catch(err => console.log("error = ", err));
 
   })
   .post("/register", (req, res) => {
