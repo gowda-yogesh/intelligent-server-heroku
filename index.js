755623b7d40a170aefdd38ea1da8085b14c7bcd9
YOGESH C GOWDA <yog.161096@gmail.com>
2020-08-22 15:13:06 +0530

    added register and profile and image api

--- index.js
+++ index.js
@@ -40,5 +40,103 @@ express()
     //     .catch(err => console.log("error = ", err));
 
   })
+  .post("/register", (req, res) => {
+
+    res.json("register page open ");
+
+    // console.log("hello its the reguster");
+
+    // const { name, password, email } = req.body;
+
+    // console.log("name", name);
+    // console.log("password", password);
+    // console.log("password type", typeof (password));
+
+    // const saltRounds = 10;
+    // const hash = bcrypt.hashSync(String(password), saltRounds);
+    // console.log(hash);
+
+    // const correct = bcrypt.compareSync("123456", hash); // true
+    // const wrong = bcrypt.compareSync("preetu", hash); // false
+
+    // console.log("correct", correct);
+    // console.log("worng", wrong);
+
+    // db.transaction(trx => {
+    //     trx.insert({
+    //         hash: hash,
+    //         email: email
+    //     })
+    //         .into('login')
+    //         .returning('email')
+    //         .then(loginEmail => {
+    //             //return is extremely imp else program hangs;
+    //             return trx('users')
+    //                 .returning('*')
+    //                 .insert({
+    //                     email: loginEmail[0],
+    //                     name: name,
+    //                     joined: new Date()
+    //                 })
+    //                 .then(user => { res.json(user[0]) })
+    //         })
+    //         //most important line trx.commit
+    //         .then(trx.commit)
+    //         .catch(trx.rollback)
+
+
+    // }).catch(err => { res.status(400).json("could not register") });
+
+  })
+
+  .get("/profile/:id", (req, res) => {
+
+    res.json("profile and id as params page open ");
+
+    // console.log("profile");
+    // const { id } = req.params;
+    // console.log("id", id);
+
+    // db
+    //   .select('*')
+    //   .from('users')
+    //   .where({ id: id })
+    //   .then(user => {
+    //     if (user.length > 0) {
+    //       res.json(user);
+    //     } else {
+    //       res.status(400).json("Invalid ");
+    //     }
+    //   })
+    //   .catch(err => res.status(400).json("Error: Sorry cant respond to ur request "));
+
+
+  })
+
+  .put("/image", (req, res) => {
+
+    res.json("image page open ");
+
+    // console.log("image");
+    // const { id } = req.body;
+    // console.log(req.body);
+    // console.log("id", id);
+
+    // let found = false;
+    // dataBase.map((person) => {
+    //   console.log("map");
+    //   console.log(person);
+    //   if (person.id == id) {
+    //     found = true;
+    //     ++person.rank
+    //     res.json(person);
+    //     return;
+    //   }
+    // })
+    // if (found === false) {
+    //   res.status(404).json("was unsucessfull response ")
+    // }
+  })
+
 
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
